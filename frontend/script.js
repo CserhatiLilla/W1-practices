41b2a18714aa209fd06c72c8e8a451b4dbb2d78b
CserhatiLilla <lillacserhati2@gmail.com>
2022-04-07 22:05:35 +0200

    ez

---
+++ frontend/script.js
@@ -0,0 +1,25 @@
+console.log("ez az elso javascriptem")
+/*
+// a stringeket (szoveg) idezojelbe kell rakni
+"ez itt egy szoveg"
+// a dupla es sima idezojel csak egysoros stringet enged meg
+'ez itt egy masik szoveg'
+// backtickek koze rakott stringekben lehet sortores
+`ez egy tobb 
+soros string`
+
+4 
+
+128
+
+00013
+// a szamokat nem kell idezojelbe rakni
+04
+*/
+1.13
+
+function myFirstFunction(){
+    console.log("ez az elso fuggvenyem belsejeben levo log")
+}
+
+myFirstFunction()
